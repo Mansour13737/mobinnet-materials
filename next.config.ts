f3ee2b5435003330d2ffe2cb1248e455c1baea68
Mansour nosrati <mansournosrati69@gmail.com>
2025-12-09 21:43:42 +0000

    پروژه روی گیت هاب پوش شد و از اونجا روی ورسل دیپلوی شده اما وقتی صفحه ای

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  },
 };
 
 export default nextConfig;
